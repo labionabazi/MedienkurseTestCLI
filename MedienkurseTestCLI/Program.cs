6fe3a75af0d3aa0bc974ac6be8d27b3ffaa4bf27
Labion Abazi <Labion.Abazi@swisscom.com>
2017-12-05 14:20:30 +0100

    Add project files.

---
+++ MedienkurseTestCLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MedienkurseTestCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
